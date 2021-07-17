9f064e1f3a2e9fc7fc95a434e0f77b2f6fb67a22
Amy <amy@2206.design>
2021-07-17 13:42:40 -0400

    Added screenshot placeholder and enqueued styles and scripts

--- functions.php
+++ functions.php
@@ -9,10 +9,14 @@
  * Enqueue theme's scripts and styles
  */
 	function wpdough_enqueue_scripts() {
-			wp_enqueue_style('main-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-			// wp_enqueue_script('script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+		// Register styles and scripts
+		wp_register_style('main-css', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+		wp_register_script('main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime(get_template_directory() . '/assets/js/main.js'), true );
+
+		// Enqueue styles and scripts
+		wp_enqueue_style('main-css');
+		wp_enqueue_script('main-js');
 	}
 
 	add_action('wp_enqueue_scripts', 'wpdough_enqueue_scripts');
-
 ?>
