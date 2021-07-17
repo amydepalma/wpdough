358fa7c8e96e36946c4694a9b8c473feba56f4f1
Amy <amy@2206.design>
2021-07-17 10:20:25 -0400

    Various function additions

--- header.php
+++ header.php
@@ -7,13 +7,15 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="<?php language_attributes(); ?>">
 <head prefix="og: http://ogp.me/ns#">
-	<meta charset="UTF-8">
+	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>WPdough</title>
+	<?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+
 <header>Header</header>
