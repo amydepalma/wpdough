d58606b1c64bd249a9045598edc3bb3fb08e99a6
Amy DePalma <amydepalma@gmail.com>
2023-12-27 00:36:28 -0500

    Added updated breakpoints and some type sizing

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<!-- TODO MAKE THIS BETTER INCLUDED  VIA SELF-HOSTING-->
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,700;1,300&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,500;0,600;1,300&display=swap" rel="stylesheet">
 		<?php wp_head() ?>
 	</head>
 
